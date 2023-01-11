2d8c1e0bc4cfda040f4c65529dc9e9e34dc489ca
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2023-01-11 15:58:11 +0100

    buld component when i can add new task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
+import { AddTaskComponent } from './component/add-task/add-task.component';
+import { TaskComponent } from './component/task/task.component';
+import { TasksComponent } from './component/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent,
+    AddTaskComponent,
+    TaskComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
